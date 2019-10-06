2abbf5b94f0f1b2e0e9aca9f8da9c838fd461bfb
KepocuH <mkv20000@mail.ru>
2019-10-06 19:57:15 +0700

    create stdafx.h

---
+++ stdafx.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <iterator>
+#include <fstream>
+#include <map>
+#include <cmath>
+#include <set>
+#include <stack>
