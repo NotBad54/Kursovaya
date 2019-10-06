413e22ffd1bcda806524ea88811629bba8c24f97
KepocuH <mkv20000@mail.ru>
2019-10-06 13:14:38 +0700

    Update main.cpp fix #2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
-#include <iostream>
-#include <fstream>
-#include <string>
+﻿﻿#include <iostream>
 #include <vector>
 #include <algorithm>
+#include <iterator>
+#include <string>
+#include <fstream>
+#include <map>
+#include <cmath>
+#include <set>
+#include <stack>
+#include <stdlib.h>
+
 
 
 using namespace std;
@@ -10,4 +17,36 @@ using namespace std;
 
 int main()
 {
+tokens texpr, pexpr;
+	Variables expvars;
+	Massives expmasvars;
+	string expr;
+	ifstream file("test.txt");
+
+	ReadExpressionFromStream(file, expr, expvars, expmasvars);
+
+	cout << "Expr:" << endl;
+	cout << expr << endl;
+
+	Variables::iterator it;
+	for (it = expvars.begin(); it != expvars.end(); it++)
+		cout << it->first << '=' << it->second << endl;
+
+	Massives::iterator it1;
+	for (it1 = expmasvars.begin(); it1 != expmasvars.end(); it1++) {
+		cout << it1->first << '{';
+		for (int i = 0; i < it1->second.size(); i++) {
+			if (i == it1->second.size() - 1)
+				cout << it1->second[i];
+			else
+				cout << it1->second[i] << ',';
+		}
+		cout << '}' << endl;
+	}
+	cout << endl;
+
+	cout << ResultExpr(pexpr, expvars, expmasvars) << endl;
+
+	system("PAUSE");
+	return 0;
 }
