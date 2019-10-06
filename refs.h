e3e2a795766c473ac259989d9938a5d8fb26cef8
KepocuH <47728345+KepocuH@users.noreply.github.com>
2019-10-06 21:34:02 +0700

    Update refs.cpp refs.h fix #2

--- refs.h
+++ refs.h
@@ -1,17 +1,13 @@
 #pragma once
 #ifndef _REFS_H
 #define _REFS_H
-#include <string>
-#include <fstream>
-#include <vector>
-#include <map>
-
-
+#include "stdafx.h"
 
 using namespace std;
 
 typedef map<string, double> Variables;
 typedef map<string, vector<double> > Massives;
+
 void ReadExpressionFromStream(ifstream &inp, string &expr, Variables &var, Massives &mas);
 
 #endif
